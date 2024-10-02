160b62da0279fa651646a3a7a9c588295337cb9b
Vitor <miconimacerino@gmail.com>
2024-10-02 14:46:12 -0300

    componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        minhaFonte: ['AKZI', 'sans-serif'],
+      },
+      colors: {
+        laranja: '#ff7110',
+        azul: '#0066cc',
+        verde: '#00a204',
+        cinza: '#7c7b7b',
+      },
+    },
   },
   plugins: [],
 }
